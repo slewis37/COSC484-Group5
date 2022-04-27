af2eb79f6ef14afb0202608d478ef6b874c5f7f1
alexTU1 <95954794+alexTU1@users.noreply.github.com>
2022-04-26 17:30:15 -0700

    Update navbar_component.js

--- client/src/components/navbar_component.js
+++ client/src/components/navbar_component.js
@@ -15,36 +15,34 @@ const Navbar = () => {
         <header className="header">
             <div className="container2">
                 <div className="row space-between algin-items-center">
-                    <div className="logo">
-                        <h2>cheapFoodie</h2>
-                    </div>
+                    {/* <div className="logo"> */}
+                        <h2 className="logo" >Eazy Eats <em className = "tuColor">TU</em></h2>
+                    {/* </div> */}
 
-        <div className = "nav">
-            <ul className='nav-links'>
-    <li>
-      <Link to="/" className='active'>Home</Link>
-    </li>
-    <li>
-      <Link to="/recipeView">Recipies</Link>
-    </li>
-    <li>
-      <Link to="/cuisines">Cuisines</Link>
-    </li>
-    <li>
-      <Link to="/drinks">Drinks</Link>
-    </li>
-    <li>
-      <Link to="/support">Support</Link>
-    </li>
-    </ul>
-  </div>
+                    {/* <div className = "nav"> */}
+                        <ul className='nav-links'>
+                          <li>
+                            <Link to="/" className='active'>Home</Link>
+                          </li>
+                          <li>
+                            <Link to="/recipeView">Recipies</Link>
+                          </li>
+                          <li>
+                            <Link to="/inventory">My Inventory</Link>
+                          </li>
+                          <li>
+                            <Link to="/recipeSearch">Search</Link>
+                          </li>
+                          {/* <li>
+                            <Link to="/recipeView">Recipes</Link>
+                          </li> */}
+                        </ul>
+                      {/* </div> */}
 
-  <div className="profile-img">
-                        
-                        <img src="https://www.pngkey.com/png/full/202-2024691_my-profile-comments-my-profile-icon-png.png " alt=" "/>
-                    
-                </div>
-            </div>
+                      {/* <div className="profile-img">                 */}
+                          <img className="profile-img" src="https://www.pngkey.com/png/full/202-2024691_my-profile-comments-my-profile-icon-png.png " alt=" "/>        
+                      {/* </div> */}
+                  </div>
         </div>
     </header>
     );
