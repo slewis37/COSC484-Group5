71990989a8241800883aa8a8b8131a896a0d726a
dandrusko04 <84750200+dandrusko04@users.noreply.github.com>
2022-05-03 22:15:00 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ import Login from "./components/login_component.js";
 import Navbar from "./components/navbar_component.js";
 import RecipeSearch from "./components/recipeSearch_component.js";
 import RecipeView from "./components/recipeView_component.js";
-import Register from "./components/register_component.js";
+import Register from "./components/AccountRegistration/register_component.js";
 // import Support from "./components/support_component.js";
 // import Cuisines from "./components/cuisines_component.js";
 // import Drinks from "./components/drinks_component.js";
@@ -19,31 +19,40 @@ import Register from "./components/register_component.js";
 
 function App() {
     return (
-        // <>
-        //   <Navbar/>
-        //   <Home/>
-        //   <Register/>
-        //   <Login/>
-        //   <RecipeView/>
-        //   <RecipeSearch/>
-        //   <Inventory/>
-        // </>
-        <Router>
-        <Navbar/> { /* <br/> */ } 
-        <Routes >
-        <Route path = "/" exact element = { < Home/> }> </Route> 
-        <Route path = "/register" element = { < Register/> } > </Route> 
-        <Route path = "/login" exact element = { < Login/> } > </Route> 
-        <Route path = "/recipeView" exact element = { < RecipeView/> } > </Route> 
-        <Route path = "/recipeSearch" exact element = { < RecipeSearch/> } > </Route> 
-        <Route path = "/inventory" exact element = { < Inventory/> } > </Route> {
+            // <>
+            //   <Navbar/>
+            //   <Home/>
+            //   <Register/>
+            //   <Login/>
+            //   <RecipeView/>
+            //   <RecipeSearch/>
+            //   <Inventory/>
+            // </>
+            <
+            Router >
+            <
+            Navbar / > { /* <br/> */ } <
+            Routes >
+            <
+            Route path = "/"
+            exact element = { < Home / > } > < /Route>  <
+            Route path = "/register"
+            element = { < Register / > } > < /Route>  <
+            Route path = "/login"
+            exact element = { < Login / > } > < /Route>  <
+            Route path = "/recipeView"
+            exact element = { < RecipeView / > } > < /Route>  <
+            Route path = "/recipeSearch"
+            exact element = { < RecipeSearch / > } > < /Route>  <
+            Route path = "/inventory"
+            exact element = { < Inventory / > } > < /Route> {
             /* <Route path="/support" exact element={<Support/>} ></Route>
                   <Route path="/cuisines" exact element={<Cuisines/>} ></Route>
                   <Route path="/drinks" exact element={<Drinks/>} ></Route> */
-        } 
-        </Routes> 
-        </Router>
-    );
+        } <
+        /Routes>  <
+        /Router>
+);
 }
 
 export default App;
